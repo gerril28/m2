eda061a3704a3300e0419b92298daceb7e5eea80
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-22 09:28:06 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let number1 = 0
+let number = 0
 let A = 0
 let B = 0
 let C = 0
@@ -10,29 +10,38 @@ let H = 0
 let I = 0
 let J = 0
 input.onButtonPressed(Button.A, function () {
-    number1 += 1
-    basic.showNumber(number1)
+    number += 1
+    basic.showNumber(number)
 })
 input.onGesture(Gesture.LogoUp, function () {
-    basic.showNumber(0)
-    basic.showNumber(0)
-    basic.showNumber(0)
-    basic.showNumber(0)
-    basic.showNumber(0)
+    basic.showNumber(A)
+    basic.showNumber(B)
+    basic.showNumber(C)
+    basic.showNumber(D)
+    basic.showNumber(E)
+    basic.showNumber(F)
+    basic.showNumber(G)
+    basic.showNumber(H)
+    basic.showNumber(I)
+    basic.showNumber(J)
+})
+input.onButtonPressed(Button.AB, function () {
+    let number1 = 0
+    number = number1
 })
 input.onButtonPressed(Button.B, function () {
-    number1 += -1
-    basic.showNumber(number1)
+    number += -1
+    basic.showNumber(number)
 })
 input.onGesture(Gesture.Shake, function () {
-    A = number1 + 9
-    B = number1 - -9
-    C = number1 * 2
-    D = number1 / 3
-    E = number1 % 4
-    F = Math.sqrt(number1)
+    A = number + 9
+    B = number - -9
+    C = number * 2
+    D = number / 3
+    E = number % 4
+    F = Math.sqrt(number)
     G = Math.round(3 / 4)
     H = Math.ceil(3 / 9)
-    I = number1 ** 3
+    I = number ** 3
     J = randint(0, 100)
 })
